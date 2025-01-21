47f644df99c2e2d601729b3a22b5170a313892be
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2025-01-21 11:24:04 +0100

    test(firestore/document_data_source): add basic test

---
+++ terraform-provider-firestore/internal/provider/document_data_source_test.go
@@ -0,0 +1,66 @@
+package provider
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccFirestoreDocument_basic(t *testing.T) {
+	projectID, projectIDSet := os.LookupEnv("TF_TEST_GCP_PROJECT_ID")
+	if !projectIDSet {
+		t.Fatalf("TF_TEST_GCP_PROJECT_ID not set")
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"firestore": providerserver.NewProtocol6WithError(New()()),
+		},
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"google": {VersionConstraint: "6.16.0", Source: "google"},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+resource "google_firestore_document" "test" {
+  project     = %q
+  database    = "test-firestore-infra" 
+  collection  = "provider"
+  document_id = "basicTest"
+  fields      = "{\"key1\":{\"stringValue\":\"avalue\"}, \"key2\":{\"integerValue\":\"5\"}, \"key3\":{\"booleanValue\":true}}"
+}
+
+data "firestore_document" "test" {
+  project = %q
+  database = google_firestore_document.test.database
+  collection = google_firestore_document.test.collection
+  document_id = google_firestore_document.test.document_id
+}`, projectID, projectID),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.firestore_document.test",
+						tfjsonpath.New("fields").AtMapKey("key1"),
+						knownvalue.StringExact("avalue"),
+					),
+					statecheck.ExpectKnownValue(
+						"data.firestore_document.test",
+						tfjsonpath.New("fields").AtMapKey("key2"),
+						knownvalue.Int64Exact(5),
+					),
+					statecheck.ExpectKnownValue(
+						"data.firestore_document.test",
+						tfjsonpath.New("fields").AtMapKey("key3"),
+						knownvalue.Bool(true),
+					),
+				},
+			},
+		},
+	})
+}
